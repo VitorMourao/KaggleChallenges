15023e7ed9f2a7948e191f37e874857215b25ac3
Vitor Mourão <46322896+VitorMourao@users.noreply.github.com>
2024-06-18 16:17:27 -0300

    Uploaded data_preprocessing.py

---
+++ Titanic/scripts/data_preprocessing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 15:59:18 2024
+
+@author: Vitor Hugo Mourão & Natália dos Reis
+"""
+
+import pandas as pd
+from utils.io_utils.py import load_data, save_data
+
+def preprocess_data(filepath):
+    df = load_data(filepath)
+    # Add preprocessing steps here
+    save_data(df, 'data/processed/train_processed.csv')
---
+++ Titanic/utils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 16:05:42 2024
+
+@author: Vitor Hugo Mourão & Natália dos Reis
+"""
+
+# utils/__init__.py
+# This file can be empty
