8fb87cde9e19520ce32227af977ce12dc666d1b3
Vitor Mourão <46322896+VitorMourao@users.noreply.github.com>
2024-06-18 18:26:31 -0300

    Removed titanic version 0

--- Titanic/scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 18 16:05:06 2024
-
-@author: Vitor Hugo Mourão & Natália dos Reis
-"""
-
-# scripts/__init__.py
-# This file can be empty
--- Titanic/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 18 16:05:42 2024
-
-@author: Vitor Hugo Mourão & Natália dos Reis
-"""
-
-# utils/__init__.py
-# This file can be empty
