0d27ad0e6a9385dea294334ff9ac827a885d0799
Vitor Mourão <46322896+VitorMourao@users.noreply.github.com>
2024-07-07 17:34:14 -0300

    Updated main and model_01

--- Titanic/main.py
+++ Titanic/main.py
@@ -13,46 +13,31 @@ ipython.run_line_magic('autoreload', '2') # Setting 'autoreload' to mode 2, relo
 
 # Importing necessary modules
 import os
+from config import train_data_path, train_data_processed, test_data_path, test_data_processed, test_predict_path, model_eval_path
+from config import train_preprocessing_params, test_preprocessing_params
 from utils.io_utils import load_data, save_data
 from scripts.data_preprocessing import preprocess_data
 from models.fem_survived_male_died import women_first
 from models.logistic_regression import train_logistic_regression, test_logistic_regression
 
-# Defining the paths to the training and test data files
-train_data_path = os.path.join('data', 'raw', 'train.csv')
-test_data_path = os.path.join('data', 'raw', 'test.csv')
-
-# Defining the path to the processed training data file
-train_data_processed = os.path.join('data', 'processed', 'train_processed.csv')
-test_data_processed = os.path.join('data', 'processed', 'test_processed.csv')
-
-# Folder to test predictions
-test_predict_path = os.path.join('outputs', 'test_predictions')
-# Folder to Model Evaluations
-model_eval_path = os.path.join('outputs', 'model_evaluations')
-
 # If this script is being run as the main program
 if __name__ == '__main__':
-    data_old = load_data(train_data_path)
-    preprocess_data(train_data_path, train_data_processed, drop_columns = ['Cabin'],
-                    fillna_columns={'Embarked':'mode', 'Age':'knn'}, n_neighbors=5,
-                    normalize_columns = ['Age','SibSp','Parch','Fare'], method = 'std') # preprocess the training data
-    data_new = load_data(train_data_processed)
+    train_old = load_data(train_data_path)
+    preprocess_data(train_data_path, train_data_processed, **train_preprocessing_params) # preprocess the training data
+    train_new = load_data(train_data_processed)
     
     # Save the test dataset
     test_old = load_data(test_data_path)
-    preprocess_data(test_data_path, test_data_processed, drop_columns = ['Cabin'],
-                    fillna_columns={'Fare':'mean', 'Age':'knn'}, n_neighbors=5,
-                    normalize_columns = ['Age','SibSp','Parch','Fare'], method = 'std')
+    preprocess_data(test_data_path, test_data_processed, **test_preprocessing_params)
     test_new = load_data(test_data_processed)
 
     # Implement the first model (Only women survive)
-    model_01_df = women_first(test_old) # Does not need any training
+    model_01_df = women_first(train_old, test_old) # Does not need any training
     model_01_df_data_path = os.path.join(test_predict_path, 'test_01.csv')
     save_data(model_01_df, model_01_df_data_path)
     
     # Implement Logistic Regression 
-    model_02 = train_logistic_regression(data_new)
+    model_02 = train_logistic_regression(train_new)
     model_02_df = test_logistic_regression(model_02, test_new)
     model_02_df_data_path = os.path.join(test_predict_path, 'test_02.csv')
     save_data(model_02_df, model_02_df_data_path)
--- Titanic/models/fem_survived_male_died.py
+++ Titanic/models/fem_survived_male_died.py
@@ -7,7 +7,7 @@ Created on Mon Jul  1 18:19:21 2024
 
 from utils.clean_test import reduce_test_df
 
-def women_first(df):
+def women_first(train, test):
     """
     Assigns survival predictions based on the 'women and children first' principle.
     
@@ -17,12 +17,24 @@ def women_first(df):
     - 0 for male passengers (indicating no survival)
     
     Parameters:
-    test_df (pd.DataFrame): A DataFrame containing the test data with at least a 'Sex' column.
+    train (pd.DataFrame): A DataFrame containing the training data with at least a 'Sex' and 'Survived' column.
+    test (pd.DataFrame): A DataFrame containing the test data with at least a 'Sex' column.
     
     Returns:
     pd.DataFrame: The input DataFrame with an additional 'Survived' column filled with survival predictions.
     """
-    test_df = df.copy()
+    
+    # Predict survival for the training data
+    train_df = train.copy()
+    train_df['PredictedSurvived'] = 0  # Initialize all passengers as not survived
+    train_df.loc[train_df['Sex'] == 'female', 'PredictedSurvived'] = 1  # Set 'PredictedSurvived' to 1 for female passengers
+    
+    # Calculate accuracy
+    accuracy = (train_df['Survived'] == train_df['PredictedSurvived']).mean()
+    print(f"Accuracy on training data: {accuracy * 100:.2f}%")
+    
+    # Predict survival for the test data
+    test_df = test.copy()
     test_df['Survived'] = 0 # Initialize all passengers as not survived
     test_df.loc[test_df['Sex'] == 'female', 'Survived'] = 1 # Set 'Survived' to 1 for female passengers
     test_df = reduce_test_df(test_df)
